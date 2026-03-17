b203a504b74c284ce6b290abfc5bcb3f1857dbc7
BurinJ <160215837+BurinJ@users.noreply.github.com>
2026-03-17 10:48:43 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,8 @@
 # --- CONFIGURATION ---
-PROMETHEUS_URL = "http://158.108.4.150:9090"
-PROM_USER = "admin"
-PROM_PASSWORD = "Z73F1R1BocTHwyv"
-HOSTNAME_MAPPING = {
-    # "database_hostname": "Friendly Name",
-    # "OCS-F7-DEV": "สำนักบริการคอมพิวเตอร์ ชั้น 7 (Dev)",
-    # "OCS-F7": "สำนักบริการคอมพิวเตอร์ ชั้น 7",
-    # "LIB-LIB1-F1": "สำนักหอสมุด ชั้น 1",
-    # "50Y-F4": "สำนักงานอธิการบดี 50 ปี ชั้น 4",
-    # "50Y-F5": "สำนักงานอธิการบดี 50 ปี ชั้น 5",
-    
-}
+# CONTACT OFFICE OF COMPUTER SERVICE FOR THE DATABASE
+PROMETHEUS_URL = ""
+PROM_USER = ""
+PROM_PASSWORD = ""
+
+# --- MANUAL MAPPING ---
+HOSTNAME_MAPPING = {}
